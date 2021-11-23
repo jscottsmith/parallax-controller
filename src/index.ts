4c052c6b9a0be6fa4cf32acc0315f42ea15298d6
J Scott Smith <jscsmith@gmail.com>
2021-11-22 23:56:11 -0800

    export classes

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,34 @@
-export const sum = (a: number, b: number) => {
-  if ('development' === process.env.NODE_ENV) {
-    console.log('boop');
-  }
-  return a + b;
+import { Bounds } from './classes/Bounds';
+import { Element } from './classes/Element';
+import { ParallaxController } from './classes/ParallaxController';
+import { Rect } from './classes/Rect';
+import { Scroll } from './classes/Scroll';
+import { View } from './classes/View';
+import { setParallaxStyles, resetStyles } from './helpers/elementStyles';
+import { getOffsets } from './helpers/getOffsets';
+import { getParallaxOffsets } from './helpers/getParallaxOffsets';
+import { isElementInView } from './helpers/isElementInView';
+import { percentMoved } from './helpers/percentMoved';
+import { createId } from './utils/createId';
+import { parseValueAndUnit } from './utils/parseValueAndUnit';
+import { scaleBetween } from './utils/scaleBetween';
+import { testForPassiveScroll } from './utils/testForPassiveScroll';
+
+export {
+  Bounds,
+  Element,
+  ParallaxController,
+  Rect,
+  Scroll,
+  View,
+  setParallaxStyles,
+  resetStyles,
+  getOffsets,
+  getParallaxOffsets,
+  isElementInView,
+  percentMoved,
+  createId,
+  parseValueAndUnit,
+  scaleBetween,
+  testForPassiveScroll,
 };
