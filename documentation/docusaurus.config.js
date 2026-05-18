160214ca0528374d85e54f8bcda9c8c9092e8af4
J Scott Smith <jscsmith@gmail.com>
2026-05-18 11:29:22 -0700

    deps: update docusaurus to 3.0

--- documentation/docusaurus.config.js
+++ documentation/docusaurus.config.js
@@ -1,8 +1,9 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+const {themes} = require('prism-react-renderer');
+const lightCodeTheme = themes.github;
+const darkCodeTheme = themes.dracula;
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
@@ -12,7 +13,11 @@ const config = {
   url: 'https://parallax-controller.damnthat.tv',
   baseUrl: '/',
   onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
+  markdown: {
+    hooks: {
+      onBrokenMarkdownLinks: 'warn',
+    },
+  },
   favicon: 'img/favicon.ico',
   organizationName: 'jscottsmith', // Usually your GitHub org/user name.
   projectName: 'parallax-controller', // Usually your repo name.
