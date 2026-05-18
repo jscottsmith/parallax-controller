6326a69ed17c2c8db72a55b60368f52649904cc4
J Scott Smith <jscsmith@gmail.com>
2026-05-18 12:21:11 -0700

    docs: v2 beta

--- documentation/src/pages/index.tsx
+++ documentation/src/pages/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import {Redirect} from 'react-router-dom';
+import { Redirect } from 'react-router-dom';
 
 export default function Home() {
-  return <Redirect to="/docs/next/intro" />;
+  return <Redirect to="/docs/v2/intro" />;
 }
